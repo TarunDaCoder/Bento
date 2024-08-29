c7a4df6e19b00d5c9cbba3684dd780ca4660302d
TarunDaCoder <tarunsnair09@gmail.com>
2024-08-29 16:58:22 +0000

    feat: update config

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
-	imageBackground: false,
+	name: 'Tarun',
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
